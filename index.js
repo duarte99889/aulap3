aff8a955519ffe12aa95d4656b202113adb8945f
duarte99889 <duarte00@icloud.com>
2024-10-02 10:32:22 +0100

    mudanças index

---
+++ index.js
@@ -0,0 +1,10 @@
+<ol>
+  <li> Teste de prettier</li>
+  <li>mais um teste</li>
+  <li>maais um teste</li>
+  <li> Teste</li>
+  <li> Teste de prettier</li>
+  <li>maais um teste</li>
+  <li> Teste de prettier</li>
+  <li>maais um teste</li>
+</ol>;
